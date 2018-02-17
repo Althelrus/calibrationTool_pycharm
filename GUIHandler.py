f226263504cbd20f6d613a718c94b0fe0e9fc6b5
Althelrus <Althelrus@gmail.com>
2018-02-17 17:16:06 -0500

    Big Update... to fix GUIHandler and the main tasks. The GUIHandler now handles all GUI items and the main does all the calculation to balance the AUV.

--- GUIHandler.py
+++ GUIHandler.py
@@ -1,8 +1,15 @@
-#By: Jonathan Ramirez
-#Company: University of New Haven
+# By: Jonathan Ramirez
+# Company: University of New Haven
+
+# Import of basic System Functions
+import sys
+import os
+import time
+import logging
 
 from PyQt4 import QtGui
 from UI.main_gui import Ui_MainWindow
+from main import Balance
 
 
 class MainOverride(QtGui.QMainWindow):
@@ -17,9 +24,18 @@ class MainOverride(QtGui.QMainWindow):
             event.accept()
 
 
-class UIHandler(Ui_MainWindow):
+class UIHandler(Ui_MainWindow, Balance):
+    app = QtGui.QApplication(sys.argv)
 
     def __init__(self):
+        super(Balance, self).__init__()
+
+        # ________ P R O C E D U R E -- B U I L D -- R E V S _____
+        self.version = '0.1'
+        self.git_build = '0 '
+        # ________________________________________________________
+
+        # Start Main Window
         self.mainWindow = MainOverride()
         self.setupUi(self.mainWindow)
         # self.MainWindow.setWindowIcon(QtGui.QIcon(os.getcwd() + '/graphics/win_icon1.png')) #To add window icon
@@ -29,8 +45,14 @@ class UIHandler(Ui_MainWindow):
         self.update_progress_bar(0)
         self.mainWindow.show()
 
+        # Link Buttons
+        self.start_calibration_btn.clicked.connect(self.start_test)
+        self.recalibration_btn.clicked.connect(self.start_test)
+        self.settings_btn.clicked.connect(self.show_setting)
+        self.back_btn.clicked.connect(self.show_main_screen)
+
     def about(self):
-        QtGui.QMessageBox.about(self.MainWindow, "About Balancing Test",
+        QtGui.QMessageBox.about(self.mainWindow, "About Balancing Test",
                                 "<b>University of New Haven <br><br>"
                                 "File Name: Balancing Test <br>Revision: {}<br>"
                                 "Build: {}<br><br><i><u>"
@@ -39,3 +61,43 @@ class UIHandler(Ui_MainWindow):
 
     def update_progress_bar(self, value):
         self.calibration_progbar.setValue(value)
+
+    def show_main_screen(self):
+        self.mainFrame.show()
+        self.logFrame.hide()
+        self.loadingFrame.hide()
+        self.settingFrame.hide()
+
+    def start_test(self):
+        self.mainFrame.hide()
+        self.loadingFrame.show()
+        self.show_loading()
+
+    def show_setting(self):
+        self.mainFrame.hide()
+        self.settingFrame.show()
+
+    def show_log(self):
+        self.mainFrame.hide()
+        self.logFrame.show()
+        self.loadingFrame.hide()
+        self.settingFrame.hide()
+
+    def show_loading(self):
+        self.update_progress_bar(0)
+        self.print_to_log(self.do_calculation())
+        for wait in range(0, 100, 1):
+            time.sleep(0.01)
+            self.update_progress_bar(wait)
+        self.show_log()
+
+    def print_to_log(self, balancing_info):
+        pass
+
+
+if __name__ == '__main__':
+    QtGui.QApplication.setStyle('cleanlooks ')
+    ex_main = UIHandler()
+    # print help(ex_main)
+    # print '----------------'
+    sys.exit(UIHandler.app.exec_())
--- main.py
+++ main.py
@@ -7,60 +7,28 @@ import os
 import time
 import logging
 
-# GUI Imports
-from PyQt4 import QtGui
-from GUIHandler import UIHandler
 
-
-# Other
-
-
-class Balance(UIHandler):
-    # ________ P R O C E D U R E -- B U I L D -- R E V S _____
-    version = '0.1'
-    git_build = '0 '
-    # ________________________________________________________
-    app = QtGui.QApplication(sys.argv)
-
+class Balance(object):
     def __init__(self):
-        super(Balance, self).__init__()
-        self.start_calibration_btn.clicked.connect(self.start_test)
-        self.recalibration_btn.clicked.connect(self.start_test)
-        self.settings_btn.clicked.connect(self.show_setting)
-        self.back_btn.clicked.connect(self.show_main_screen)
+        pass
 
-    def show_main_screen(self):
-        self.mainFrame.show()
-        self.logFrame.hide()
-        self.loadingFrame.hide()
-        self.settingFrame.hide()
+    def do_calculation(self):
+        pass
 
-    def start_test(self):
-        self.mainFrame.hide()
-        self.loadingFrame.show()
-        self.calculation()
+    def get_load_cell_one(self):
+        pass
 
-    def show_setting(self):
-        self.mainFrame.hide()
-        self.settingFrame.show()
+    def get_load_cell_two(self):
+        pass
 
-    def show_log(self):
-        self.mainFrame.hide()
-        self.logFrame.show()
-        self.loadingFrame.hide()
-        self.settingFrame.hide()
+    def find_center_of_buoyancy(self):
+        pass
 
-    def calculation(self):
-        self.update_progress_bar(0)
-        for wait in range(0, 100, 1):
-            time.sleep(0.01)
-            self.update_progress_bar(wait)
-        self.show_log()
+    def obtain_buoyancy_data(self):
+        pass
 
+    def find_center_of_mass(self):
+        pass
 
-if __name__ == '__main__':
-    QtGui.QApplication.setStyle('cleanlooks ')
-    ex_main = Balance()
-    # print help(ex_main)
-    # print '----------------'
-    sys.exit(Balance.app.exec_())
+# if __name__ == '__main__':
+#
